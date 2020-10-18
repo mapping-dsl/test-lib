43961c62fb5e7e3e1e2d663e4cb96cba95456eab
AlexeyAnufriev <contact@alexey-anufriev.com>
2020-10-19 00:49:29 +0200

    feat: create simple set of test POJOs

---
+++ README.md
@@ -0,0 +1,4 @@
+# Test 3rd-party lib
+
+Used in `mapping-dsl` project to verify generator `maven` plugin being capable
+for generation of wrapper DSL classes for 3rd-party code.
---
+++ src/main/java/org/thirdpatry/lib/SimpleDto.java
@@ -0,0 +1,15 @@
+package org.thirdpatry.lib;
+
+public class SimpleDto {
+
+    private String val;
+
+    public String getVal() {
+        return val;
+    }
+
+    public void setVal(String val) {
+        this.val = val;
+    }
+
+}
